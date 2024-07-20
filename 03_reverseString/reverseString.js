aac4005b33f211f48c4ce058141182693b897932
Moath-COE <moathabdelaziz@outlook.com>
2024-07-20 16:44:25 +0300

    Solve ex03 from js questions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (txt) {
+  let output = "";
+  for (let i = txt.length - 1; i >= 0; i--) {
+    output = output.concat(txt[i]);
+  }
+  return output;
 };
 
 // Do not edit below this line
