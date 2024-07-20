bb7507db911dbd81175d38eb2b924f22b58c4481
Moath-COE <moathabdelaziz@outlook.com>
2024-07-20 16:51:47 +0300

    Solve ex04 from js qeustions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  let output = [];
+  for (let x of arr) {
+    if (!num.includes(x)) {
+      output.push(x);
+    }
+  }
+  return output;
 };
 
 // Do not edit below this line
