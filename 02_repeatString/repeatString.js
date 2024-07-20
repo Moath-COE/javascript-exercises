b1e6efa043fc1b5aaa91093235009dcd8f536a93
Moath-COE <moathabdelaziz@outlook.com>
2024-07-20 16:37:39 +0300

    Solve ex02 from js questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (txt, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let finalText = "";
+  for (let i = 0; i < num; i++) {
+    finalText = finalText.concat(txt);
+  }
+  return finalText;
 };
 
 // Do not edit below this line
