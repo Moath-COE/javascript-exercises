6b415eae3564a2dfe903458ba955b85c91e12945
Moath-COE <moathabdelaziz@outlook.com>
2024-07-21 16:59:59 +0300

    Solve ex09 from js questions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (txt) {
+  let txtArray = txt
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .split(" ")
+    .join("")
+    .split("");
+  let txtReverse = txt
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .split(" ")
+    .join("")
+    .split("")
+    .reverse();
+  let isTrue = true;
+  for (let i in txtArray) {
+    if (txtArray[i] != txtReverse[i]) {
+      isTrue = false;
+      break;
+    }
+  }
+  return isTrue;
 };
 
 // Do not edit below this line
