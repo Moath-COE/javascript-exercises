7fde77f7051d323bc705ceb947755d407cdf776e
Moath-COE <moathabdelaziz@outlook.com>
2024-07-22 15:28:38 +0300

    Solve ex10 from js questions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  let prevNum = 0;
+  let nextNum = 1;
+  let tmp;
+  for (let i = 0; i < num; i++) {
+    tmp = nextNum;
+    nextNum = nextNum + prevNum;
+    prevNum = tmp;
+  }
+  return prevNum;
 };
 
 // Do not edit below this line
