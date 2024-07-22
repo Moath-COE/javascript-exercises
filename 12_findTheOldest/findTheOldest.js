e54e7877e4ccfa138f4e7b2b5323c5d60a004a90
Moath-COE <moathabdelaziz@outlook.com>
2024-07-22 16:12:04 +0300

    Solve ex12 from js questions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let res = arr.sort((p1, p2) => {
+    let p1Death = p1.yearOfDeath;
+    let p2Death = p2.yearOfDeath;
+    if (!("yearOfDeath" in p1)) {
+      p1Death = new Date().getFullYear();
+    }
+    if (!("yearOfDeath" in p2)) {
+      p2Death = new Date().getFullYear();
+    }
+    let p1Age = p1Death - p1.yearOfBirth;
+    let p2Age = p2Death - p2.yearOfBirth;
+    return p1Age - p2Age;
+  });
+  return res[res.length - 1];
 };
 
 // Do not edit below this line
